5d0bfb954c4488e7ff820964c165bd7026758985
asd-alo <1309948728@qq.com>
2026-08-31 21:43:13 +0800

    修改网站内容

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
       now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-      return "你好！服务器当前时间是 " + now
+      return "个人练习 " 
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
