87293c85c2ce40dc7293c5f08eb96d64dbcb8f2e
asd-alo <1309948728@qq.com>
2026-08-31 23:45:49 +0800

    fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask，request
+from flask import Flask, request
 import pymysql
 import os
 import time
@@ -65,5 +65,5 @@ def index():
 
 init_db()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
